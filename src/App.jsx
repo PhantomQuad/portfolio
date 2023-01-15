32065c6256371dd3527890f94a671a35ca57db20
nathan.anderson <nathan.anderson@rsvp.co.uk>
2023-01-15 22:23:01 +0000

    removed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import "./app.css";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <div id="home" className="bg-1 text-center text-center-vh">
+        <div>
+          Hello, I'm <span class="highlight">Nathan Anderson</span>.
+          <br />
+          I'm a web developer.
+          <a href="#experience" class="arrow">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+      </div>
+      <div id="experience" className="bg-2 text-center">
+        <div>
+          <h1 className="highlight">Experience</h1>
+        </div>
+      </div>
     </>
   );
 }
