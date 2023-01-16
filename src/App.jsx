a503b5bac87d0fb6267686ecb91f3fad4d0b9d20
nathan.anderson <nathan.anderson@rsvp.co.uk>
2023-01-16 18:33:39 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./app.css";
+import Table from "react-bootstrap/Table";
+import { Col, Container, Image, Row, Stack } from "react-bootstrap";
 
 function App() {
   return (
@@ -16,8 +18,60 @@ function App() {
         </div>
       </div>
       <div id="experience" className="bg-2 text-center">
-        <div>
-          <h1 className="highlight">Experience</h1>
+        <div class="container">
+          <h1 className="highlight mb-5">Experience</h1>
+          <div class="row align-items-start row-cols-4 mb-5">
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/bootstrap.png" />
+              Bootstrap
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/html-coding.png" />
+              HTML
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/css.png" />
+              CSS
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/javascript.png" />
+              Javascript
+            </div>
+          </div>
+          <div class="row align-items-center row-cols-4 mb-5">
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/debug.png" />
+              Debugging
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/github.png" />
+              Github
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/mongo.png" />
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/oop.png" />
+              Object-oriented Programming
+            </div>
+          </div>
+          <div class="row align-items-center row-cols-4 mb-5">
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/node.png" />
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/pair.png" />
+              Pair Programming
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/regex.png" />
+              RegEX
+            </div>
+            <div class="col">
+              <Image className="pro-image" src="/assets/img/tdd.png" />
+              Test-driven development
+            </div>
+          </div>
         </div>
       </div>
     </>
