509636cec7dbad6a62dbc8e131ea64f17503fb16
nathan.anderson <nathan.anderson@rsvp.co.uk>
2023-01-31 18:19:51 +0000

    Jenkins DSL added

---
+++ jobs/DeployApp.groovy
@@ -0,0 +1,55 @@
+pipelineJob("Deploy-Portfolio"){
+    description("Deploys portfolio site to AWS")
+    logRotator {
+        daysToKeep(5)
+        numToKeep(20)
+    }
+    concurrentBuild(allowConcurrentBuild = false)
+    triggers {
+        scm("* * * * *"){
+            ignorePostCommitHooks(ignorePostCommitHooks = false)
+        }
+    }
+    parameters {
+      stringParam("UNIQUE_IDENTIFIER", defaultValue = "portfolioaws", description = "Unique identifier!")
+    }
+    definition {
+    cpsScm {
+      scm {
+        git {
+          branch("development")
+          remote {
+            credentials("${GIT_USER}")
+            url("${GIT_URL}")
+          }
+        }
+      }
+      scriptPath('Jenkinsfile')
+    }
+  }
+}
+pipelineJob("Destroy-Portfolio"){
+    description("Destroys portfolio site on AWS")
+    logRotator {
+        daysToKeep(5)
+        numToKeep(20)
+    }
+    concurrentBuild(allowConcurrentBuild = false)
+    parameters {
+      stringParam("UNIQUE_IDENTIFIER", defaultValue = "portfolioaws", description = "Unique identifier!")
+    }
+    definition {
+    cpsScm {
+      scm {
+        git {
+          branch("master")
+          remote {
+            credentials("${GIT_USER}")
+            url("${GIT_URL}")
+          }
+        }
+      }
+      scriptPath('destroy.Jenkinsfile')
+    }
+  }
+}
