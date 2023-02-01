63f46a5f090cb301211ed9459fdce027c42bec12
nathan.anderson <blade1200@gmail.com>
2023-02-01 02:48:34 +0000

    tf tweaks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
-module "dpg_novmeber_elb" {
+module "dpg_elb" {
   source                   = "./modules/elastic_load_balancer"
   security_group_id        = data.aws_security_group.sg.id
   subnet_id                = data.aws_subnet.sn.id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
 
-module "dpg_november_asg" {
+module "dpg_asg" {
   source                   = "./modules/autoscaling_group"
   security_group_id        = data.aws_security_group.sg.id
   subnet_id                = data.aws_subnet.sn.id
-  elb_id                   = module.dpg_novmeber_elb.elb_id
+  elb_id                   = module.dpg_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
   ARTIFACT                 = var.ARTIFACT
 }
